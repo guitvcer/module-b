6dd2793c86d820cab9690d5473e4943ee5c0d08c
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-18 17:05:38 +0600

    make header logo as font black

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,7 +7,7 @@
       <a href="#"
          class="inline
                 py-1 px-2
-                text-xl font-bold italic text-slate-100
+                text-xl font-black italic text-slate-100
                 bg-slate-600 hover:bg-slate-700 dark:bg-zinc-900 hover:dark:bg-black
                 rounded-tl-md rounded-br-md">
         WorldSkills: Games
