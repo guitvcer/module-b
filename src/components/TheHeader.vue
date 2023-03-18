513548ff21f794a17d4f4c338287cd24919ea2fd
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-18 12:47:27 +0600

    add header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="px-10 py-2 flex justify-between items-center border-b w-full max-w-screen-lg m-auto">
+    <h1>
+      <a href="#"
+         class="inline py-1 px-2 text-xl font-bold italic text-slate-100
+                bg-slate-600 hover:bg-slate-700 rounded-tl-md rounded-br-md">
+        WorldSkills: Games
+      </a>
+    </h1>
+    <div>
+      <a href="#" class="px-2 hover:text-blue-700">
+        Sign Up
+      </a>
+      <a href="#" class="px-2 hover:text-blue-700">
+        Sign In
+      </a>
+    </div>
+  </header>
+</template>
