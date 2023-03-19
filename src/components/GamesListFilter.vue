385a11de91514000dbab39be965501e4255bda33
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-19 16:16:42 +0600

    fix games filters dark mode

--- src/components/GamesListFilter.vue
+++ src/components/GamesListFilter.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="grid grid-cols-2 gap-2 items-center">
     <label for="gamesListSortBy">Sort By:</label>
-    <select name="gamesListSortBy" id="gamesListSortBy" class="p-2 rounded-md">
+    <select name="gamesListSortBy" id="gamesListSortBy" class="p-2 rounded-md dark:bg-zinc-900">
       <option value="popular">Popularity</option>
       <option value="uploaddate">Recently Uploaded</option>
       <option value="title">Alphabetically</option>
     </select>
     <label for="gamesListSortDir">Sort direction:</label>
-    <select name="gamesListSortDir" id="gamesListSortDir" class="p-2 rounded-md">
+    <select name="gamesListSortDir" id="gamesListSortDir" class="p-2 rounded-md dark:bg-zinc-900">
       <option value="asc">Ascending</option>
       <option value="desc">Descending</option>
     </select>
