caa4c6b1055a1f7f29dbaba6299624befdaab1e9
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-19 18:39:45 +0600

    add home view to router

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,18 @@
   <div class="bg-slate-50 dark:bg-zinc-900 dark:text-zinc-100 h-screen">
     <div class="max-w-screen-lg m-auto bg-white dark:bg-zinc-800">
       <the-header />
-      <games-list />
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
-import GamesList from './components/GamesList.vue'
 
 export default {
   name: 'App',
   components: {
-    TheHeader,
-    GamesList
+    TheHeader
   }
 }
 </script>
