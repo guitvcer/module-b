05505cabe01a317771689991a67c9ee01a40bf0e
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-21 16:22:37 +0600

    if isAuthenticated, redirect to home from signin view

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -28,6 +28,7 @@
 import router from '@/router.js'
 import api from '@/api'
 import Logo from '@/components/Logo.vue'
+import { isAuthenticated } from '@/utils'
 
 export default {
   components: {
@@ -53,6 +54,11 @@ export default {
       localStorage.setItem('accessToken', accessToken)
 
       this.$root.$refs.alert.showAlert('success', 'You are signed in.', 'Success')
+      await router.push({ name: 'home' })
+    }
+  },
+  async mounted() {
+    if (isAuthenticated()) {
       await router.push({ name: 'home' })
     }
   }
