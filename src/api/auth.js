83efdab7a22d9655c60cf9d25207e88de7c2246d
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-21 16:17:19 +0600

    add signup

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,33 @@
 import { api_url } from '@/settings'
 
 export default {
+  async signUp(username, password) {
+    const response = await fetch(
+      `${api_url}/auth/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      }
+    )
+    const responseJson = await response.json()
+
+    if (response.status === 400) {
+      const error = new Error(responseJson.message)
+      error.status = responseJson.status
+      error.violations = responseJson.violations
+
+      throw error
+    } else if (!response.ok) {
+      throw new Error('An error has occured.')
+    }
+
+    return responseJson.token
+  },
   async signIn(username, password) {
     const response = await fetch(
       `${api_url}/auth/signin`, {
