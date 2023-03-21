0efc0cb87ed26dea1d66b336becf921d6f5dd13a
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-21 15:37:27 +0600

    pass access token in games list request

--- src/api/games.js
+++ src/api/games.js
@@ -1,11 +1,20 @@
 import { api_url } from '@/settings'
 
+const getHeaders = () => {
+  const headers = {}
+  if (localStorage.accessToken) {
+    headers.Authorization = `Bearer ${localStorage.accessToken}`
+  }
+
+  return headers
+}
+
 export default {
   async get(sortBy, sortDir) {
     const params = new URLSearchParams({
       sortBy, sortDir
     })
     const url = `${api_url}/games?${params}`
-    return fetch(url).then(response => response.json())
+    return fetch(url, {headers: getHeaders()}).then(response => response.json())
   }
 }
