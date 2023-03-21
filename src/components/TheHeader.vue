2064315770864ee5532f9991a39c2d23c7eca02a
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-21 09:23:22 +0600

    mv Logo to component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,23 +2,24 @@
   <header class="px-4 py-2
                  flex justify-between items-center
                  border-b dark:border-zinc-700">
-    <h1>
-      <router-link :to="{name: 'home'}"
-         class="inline
-                py-1 px-2
-                text-xl font-black italic text-slate-100
-                bg-slate-600 hover:bg-slate-700 dark:bg-zinc-900 hover:dark:bg-black
-                rounded-tl-md rounded-br-md">
-        WorldSkills: Games
-      </router-link>
-    </h1>
+    <logo />
     <div>
       <a href="#" class="px-2 hover:underline">
         Sign Up
       </a>
-      <a href="#" class="px-2 hover:underline">
+      <router-link :to="{name: 'signIn'}" class="px-2 hover:underline">
         Sign In
-      </a>
+      </router-link>
     </div>
   </header>
 </template>
+
+<script>
+import Logo from '@/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
