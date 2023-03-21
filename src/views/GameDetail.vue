e0b7e5aa8829663e9c762b26d0436c63f2c479fd
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-21 16:30:10 +0600

    add game detail view link

---
+++ src/views/GameDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <main-page-wrapper>
+    <main>
+    </main>
+  </main-page-wrapper>
+</template>
+
+<script>
+import MainPageWrapper from '@/components/MainPageWrapper.vue'
+
+export default {
+  components: {
+    MainPageWrapper
+  }
+}
+</script>
