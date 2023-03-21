44246dcdb01598451d16110d08955281ce12c260
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-21 15:33:15 +0600

    show Sign Out if IsAuthenticated and sign in/up if not

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,12 +4,27 @@
                  border-b dark:border-zinc-700">
     <logo />
     <div>
-      <a href="#" class="px-2 hover:underline">
+      <a
+        v-if="!isAuthenticated()"
+        href="#"
+        class="px-2 hover:underline"
+      >
         Sign Up
       </a>
-      <router-link :to="{name: 'signIn'}" class="px-2 hover:underline">
+      <router-link
+        v-if="!isAuthenticated()"
+        :to="{name: 'signIn'}"
+        class="px-2 hover:underline"
+      >
         Sign In
       </router-link>
+      <a
+        v-if="isAuthenticated()"
+        href="#"
+        class="px-2 hover:underline"
+      >
+        Sign Out
+      </a>
     </div>
   </header>
 </template>
@@ -20,6 +35,11 @@ import Logo from '@/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  methods: {
+    isAuthenticated() {
+      return Boolean(localStorage.accessToken)
+    }
   }
 }
 </script>
