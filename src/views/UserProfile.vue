71b8f638cecbe9d6f765e3bf8121145fa6930b37
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-22 16:10:30 +0600

    add empty user profile page

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,25 @@
+<template>
+  <main-page-wrapper>
+    <p></p>
+  </main-page-wrapper>
+</template>
+
+<script>
+import api from '@/api'
+import router from '@/router'
+import MainPageWrapper from '@/components/MainPageWrapper.vue'
+
+export default {
+  data() {
+    return {
+      user: null
+    }
+  },
+  components: {
+    MainPageWrapper
+  },
+  async mounted() {
+    this.user = await api.users.get(router.currentRoute.value.params.username)
+  }
+}
+</script>
