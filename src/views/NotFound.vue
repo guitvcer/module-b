0494e2b3789add4e2913e0ee589d0d81626908f8
abdrakhman <a.tazhibaev@aviata.me>
2023-04-04 16:26:41 +0600

    add NotFound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <main-page-wrapper>
+    <main>
+      <h2 class="text-2xl m-4">Page Not Found :(</h2>
+    </main>
+  </main-page-wrapper>
+</template>
+
+<script>
+import MainPageWrapper from '@/components/MainPageWrapper.vue'
+
+export default {
+  components: {
+    MainPageWrapper
+  }
+}
+</script>
